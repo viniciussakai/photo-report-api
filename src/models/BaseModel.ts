36fe0b9b14c48693b4b201b8678d065a8bbefec4
Vinicius Sakai <viniciussakai0@gmail.com>
2020-12-29 20:49:37 -0300

    feat(model): Added base Model for all models

---
+++ src/models/BaseModel.ts
@@ -0,0 +1,12 @@
+import { PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+
+export abstract class BaseModel {
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@CreateDateColumn()
+	createdAt?:Date;
+
+	@UpdateDateColumn()
+	updatedAt?:Date;
+}
